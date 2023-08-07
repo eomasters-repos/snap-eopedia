1965c9b5d57ae4e2a31f71d48ac7513eeced69da
Marco <info@eomasters.org>
2023-08-07 16:31:42 +0200

    using SNAP 10.0.0-SNAPSHOT and other minor updates

--- src/main/java/org/eomasters/eopedia/EopediaBuilder.java
+++ src/main/java/org/eomasters/eopedia/EopediaBuilder.java
@@ -11,7 +11,7 @@ public class EopediaBuilder {
     // http://192.168.178.36/mediawiki/api.php?action=opensearch&format=json&search=planet
 
     private final static Preferences PREFERENCES = Config.instance().load().preferences();
-    private static final String DEFAULT_EOPEDIA_URL = "192.168.178.36/mediawiki";
+    private static final String DEFAULT_EOPEDIA_URL = "85.215.104.64";
     private static final String FORMATTED_SEARCH_STRING = "%s%s/api.php?action=query&list=search&format=%s&srsearch=";
     //    private static final String FORMATTED_SEARCH_STRING = "/api.php?action=opensearch&format=json&search=%s";
     private static final String FORMATTED_OPEN_STRING = "%s%s/index.php?title=";
@@ -35,9 +35,9 @@ public class EopediaBuilder {
     }
 
     public Eopedia createEopedia() {
-        final String protocl = useHttp ? PROTOCOL_HTTP : PROTOCOL_HTTPS;
-        final String searchUrl = String.format(FORMATTED_SEARCH_STRING, protocl, baseUrl, FORMAT_JSON) + "%s";
-        final String openUrl = String.format(FORMATTED_OPEN_STRING, protocl, baseUrl) + "%s";
+        final String protocol = useHttp ? PROTOCOL_HTTP : PROTOCOL_HTTPS;
+        final String searchUrl = String.format(FORMATTED_SEARCH_STRING, protocol, baseUrl, FORMAT_JSON) + "%s";
+        final String openUrl = String.format(FORMATTED_OPEN_STRING, protocol, baseUrl) + "%s";
         return new Eopedia(searchUrl, openUrl);
     }
 }
